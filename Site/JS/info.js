c151407c0a327f998b6a1bed75bd2a28ad27e55a
shaunkeyd <raulbergamo416@gmail.com>
2021-09-15 19:23:00 -0300

    Atualização do js do indiv, alteração para max-width no css

---
+++ Site/JS/info.js
@@ -0,0 +1,122 @@
+const info = {
+    'Koji': {
+        'Nome': 'Koji Haruta',
+        'fotos': [
+            './album/Koji/K1.png', 
+            './album/Koji/K2.png', 
+            './album/Koji/K3.png',
+            './album/Koji/K4.png'
+        ],
+        'Descrição': `Voz de: Hiroyuki Yoshino <br> Kōji é o colega de classe de Ryuji que tem cabelo comprido. Ele é considerado um 'idiota' da classe e essa opinião é aprofundada por suas notas escolares ruins e comportamento excessivamente enérgico, afirmando coisas óbvias como o quão bonita Ami é. Suas travessuras infantis costumam ter como alvo Taiga e, como resultado, ele foi vítima de suas explosões violentas. Apesar de seu comportamento excessivamente enérgico, mais tarde na série é revelado que ele tem uma namorada.`
+    },
+    'Taiga': {
+        'Nome': 'Taiga Aisaka',
+        'fotos': [
+            './album/Taiga/T1.jpg',
+            './album/Taiga/T2.png',
+            './album/Taiga/T3.png',                         
+            './album/Taiga/T4.png'
+        ],
+        'Descrição': `Dubladora: Rie Kugimiya <br> Taiga é a protagonista feminina da série, bem como uma colega de classe de Ryūji. Devido a sua baixa estatura e ferocidade, ela foi apelidada de 'Tigresa de Bolso' (手乗りタイガー Te Nori Taigā?). Taiga fica chateada facilmente e é muito desajeitada, além de possuir uma personalidade tsundere. Devido à sua aparência fofa (descrita ser como uma boneca por Ryūji), ela frequentemente recebe muitas confissões amorosas de garotos, porém ela as rejeita. <br> Apesar de vir de uma família rica, Taiga define sua vida com seus parentes bastante complicada, por causa de diversos problemas. Ela vivia com seu pai e sua madrasta, porém, constantemente, ela brigava com eles. Assim, ela resolveu sair de casa e começar a viver por conta própria. Por vir de uma família privilegiada, ela não sabe fazer nenhuma tarefa doméstica. Perto do fim da série, os negócios de sua pai vão à falência e ele foge, deixando-a sob os cuidados de sua mãe, que já se casou novamente e tem uma nova família, a qual Taiga tem dificuldade de se aproximar. <br> Quando ela conhece Ryūji, ele acaba fazendo as tarefas domésticas de sua casa, além de cozinhar para ela. Taiga passa a maior parte do seu tempo na casa de Ryūji, tanto que é considerada um membro de sua família. No início da série, ela é apaixonada pelo melhor amigo de Ryūji, Yūsaku Kitamura, e fica nervosa toda vez que ele está por perto. Devido a isso, Ryūji e Taiga resolvem ajudar um ao outro nos objetivos amorosos.`
+    },
+    'Ryuuji': {
+        'Nome': 'Ryuuji Takasu',
+        'fotos': [
+            './album/Ryuji/R1.png',
+            './album/Ryuji/R2.jpg',
+            './album/Ryuji/R3.jpg',
+            './album/Ryuji/R4.png'
+        ],
+        'Descrição': `Dublador: Junji Majima <br> O protagonista masculino da série. Ryūji é um estudante de 17 anos que está cursando o segundo ano do ensino médio, na classe 2-C. Devido a herança biológica, seus olhos o fazem parecer um delinquente intimidador e isso causa muitos mal-entendidos. Ele é órfão de pai e por viver apenas com sua mãe, aprendeu a ser auto-suficiente. Ryūji é um bom cozinheiro e costureiro, além de gostar de manter a casa limpa. Ele é apaixonado por Minori Kushieda, uma garota da sua classe. Ele tem um periquito de animal de estimação chamado 'Inko-chan' ('Inko' significa literalmente 'periquito', em japonês). Na escola, ele é conhecido por ser 'a única pessoa capaz de parar a tigresa de bolso', referindo-se ao apelido de Taiga. <br> Ele possui baixa auto estima, por causa da sua aparência e da forma de como cresceu. Apesar disso, ele é bastante calmo na maioria das situações, além de ser responsável, inteligente e ter muitas qualidades, mesmo quando comparado ao seu melhor amigo, Yūsaku Kitamura. Nas light novels, ele vê um lado afetuoso de Taiga Aisaka que ela esconde de todos. No início, ele intimidava a maioria das pessoas, porém consegue fazer amigos ao decorrer da série.`
+    },
+    'Minori': {
+        'Nome': 'Minori Kushieda',
+        'fotos': [
+            './album/Minori/M1.jpg',
+            './album/Minori/M2.png',
+            './album/Minori/M3.png',
+            './album/Minori/M4.png'
+        ],
+        'Descrição': `Dubladora: Yui Horie <br> Minori é uma garota da classe de Ryūji, além de ser a melhor amiga de Taiga. Ela também é conhecida pelo apelido 'Minorin'. Ao contrário de Taiga, que é violenta e brava, Minori sempre está sorrindo e feliz. Ela gosta de praticar esportes, tanto que é capitã do clube de softball da escola. Minori também possui diversos empregos de tempo parcial. Ela anda em dia com a alimentação, definindo-se como uma 'guerreira da dieta'. Apesar de parecer andar com as cabeças nas nuvens, Minori é muito perspicaz, sendo capaz de ver que Ryūji, na verdade, é bastante gentil e uma das poucas pessoas a não se deixar enganar pelos atos de inocência de Ami. Com tantas emoções e ocupações, Minori mais tarde revela o amor que sentia por Ryūji.`
+    },
+    'Yuusaku': {
+        'Nome': 'Yuusaku Kitamura',
+        'fotos': [
+            './album/Yuusaku/Y1.png',
+            './album/Yuusaku/Y2.png',
+            './album/Yuusaku/Y3.png',
+            './album/Yuusaku/Y4.png'
+        ],
+        'Descrição': `Dublador: Hirofumi Nojima <br> Yūsaku é um garoto da classe de Ryūji e é seu melhor amigo. Ele é o vice-presidente do conselho estudantil, o representante da classe e o capitão do clube de softball da escola. Yūsaku usa óculos e tem uma personalidade diligente. Ele acha que não é bom em falar com as garotas, mas Ryūji acha o contrário. Durante o tempo que ficou como vice-presidente do conselho estudantil, ele acaba se apaixonando pela presidente, Sumire Kanō. Assim como Minori, ele é bastante amigável, embora possa perder o controle rapidamente. Após sua graduação, ele decide estudar na América, por causa de Sumire Kanō, por quem é apaixonado.`
+    },
+    'Ami': {
+        'Nome': 'Ami Kawashima',
+        'fotos': [
+            './album/Ami/A1.png',
+            './album/Ami/A2.png',
+            './album/Ami/A3.png',
+            './album/Ami/A4.png'
+        ],
+        'Descrição': `Dubladora: Eri Kitamura <br> Ami é uma amiga de infância de Yūsaku. Ela se transfere para a classe do amigo durante o segundo ano do ensino médio. Ela é bastante bonita e trabalha como modelo, além de ser vista como a mais madura do grupo. Depois de se transferir de escola, ela para temporariamente de trabalhar como modelo. No entanto, ela continua indo a academia para manter seu corpo. Ami é sempre gentil e amável com os outros, mas isso é apenas uma ilusão. Na verdade, ela tem uma personalidade arrogante, mas tenta esconder dos outros. Taiga e Ami não se dão muito bem, tanto que Ami sempre provoca Taiga quando tem vontade. Mas Taiga não deixa isso barato, uma vez que chama Ami de Baka-chi (Chihuahua Estúpido). Ami é bastante honesta consigo mesma, tanto que admite que é realmente cruel e que tem inveja de Taiga, uma vez que ela sempre fala a verdade para os outros. Ami brinca que está apaixonada por Ryūji, perguntando se ele poderia a amar de verdade e lhe dar uma segunda chance. Ela tem consciência do que está acontecendo entre os amigos Minori, Ryūji, Taiga e ela mesma.`
+    },
+    'Yasuko': {
+        'Nome': 'Yasuko Takasu',
+        'fotos': [
+            './album/Yasuko/Y1.png',
+            './album/Yasuko/Y2.png',
+            './album/Yasuko/Y3.png',
+            './album/Yasuko/Y4.png'
+        ],
+        'Descrição': `Dubladora: Sayaka Ōhara <br> Yasuko é a mãe de Ryūji e tem 33 anos, mas refere-se a si mesma como uma garota de 23 anos. Embora diga a Ryūji que o pai dele morreu antes de ele nascer, a verdade é que eles nunca se casaram e que ele a abandonou antes de o filho nascer. Ela trabalha em um bar chamado Bishamontengoku, onde atende pelo nome 'Mirano' (魅羅乃?), e, sozinha, sustenta a família. Dessa forma, ela deixa as tarefas domésticas para Ryūji. Yasuko gosta bastante de Taiga e a trata como um membro da família, ficando bastante chateada quando ela não aparece para o jantar. Ela possui uma personalidade infantil e é fácil de se lidar, embora fique com raiva quando tem que acordar cedo, pois geralmente chega em casa muito tarde. Ela também é muito orgulhosa por Ryuji poder ir para a escola, algo que ela não foi capaz de concluir.`
+    },
+    'Sumire': {
+        'Nome': 'Sumire Kanõ',
+        'fotos': [
+            './album/Sumire/S1.png',
+            './album/Sumire/S2.png',
+            './album/Sumire/S3.png',
+            './album/Sumire/S4.png'
+        ],
+        'Descrição': `Voz de: Yūko Kaida <br> Sumire é a presidente do conselho estudantil. Ela tem uma personalidade forte e inspira outros alunos a segui-la. Seus pais são donos de um supermercado. Ch. 49 Como ela sempre obtém as melhores notas em suas aulas a cada ano, suas notas se tornam um dos prêmios mais procurados no evento do festival da escola. Quando ela diz ao conselho que planeja deixar a escola mais cedo para estudar nos Estados Unidos para se tornar um astronauta, Yusaku fica de coração partido por tingir o cabelo e sair do conselho.Voz de: Yūko Kaida <br> `
+    },
+    'Hisamitsu': {
+        'Nome': 'Hisamitsu Noto',
+        'fotos': [
+            './album/Hisamitsu/H1.png',
+            './album/Hisamitsu/H2.png',
+            './album/Hisamitsu/H3.png',
+            './album/Hisamitsu/H4.png'
+        ],
+        'Descrição': `Voz de: Kazuyuki Okitsu <br> Hisamitsu é um aluno do sexo masculino da classe de Ryuji que usa óculos. Ele também estava na classe de Ryuji no ano anterior e se dá bem com ele. Ele é visto pela primeira vez durante o cenário de aquecimento no basquete, onde faz dupla com Minori. Noto começa a desenvolver sentimentos por Maya Kihara após uma discussão que tiveram durante a viagem de esqui de inverno.`
+    },
+    'Nanako': {
+        'Nome': 'Nanako Kashii',
+        'fotos': [
+            './album/Nanako/N1.png',
+            './album/Nanako/N2.png',
+            './album/Nanako/N3.png',
+            './album/Nanako/N4.png'
+        ],
+        'Descrição': `Voz de: Momoko Ishikawa <br> Nanako é uma aluna da classe de Ryuji que costuma sair com Maya e Ami.`
+    },
+    'Maya': {
+        'Nome': 'Maya Kihara',
+        'fotos': [
+            './album/Maya/M1.png',
+            './album/Maya/M2.png',
+            './album/Maya/M3.png',
+            './album/Maya/M4.png'
+        ],
+        'Descrição': `Voz de: Ai Nonaka <br> Maya é a atraente colega de classe de Ryuji, que costuma sair com Nanako. Ela é uma kogal e o centro do 'grupo de destaque' de meninas da classe 2-C.  Ela acaba fazendo dupla com Yusaku no aquecimento de basquete. Ela tem uma queda por Yusaku, e indiretamente se opõe à ideia de Taiga e Yusaku serem um casal, e quer que Taiga e Ryuji fiquem juntos.`
+    },
+    'Yuri': {
+        'Nome': 'Yuri Koigakubo',
+        'fotos': [
+            './album/Yuri/Y1.png',
+            './album/Yuri/Y2.png',
+            './album/Yuri/Y3.png',
+            './album/Yuri/Y4.png'
+        ],
+        'Descrição': `Voz de: Rie Tanaka <br> Yuri é a professora de sala de aula de Ryuji. Ela tem a reputação de contar longas histórias que o representante da classe Kitamura pára imediatamente para dispensar a classe. Ela gosta de fazer as coisas no seu próprio ritmo e é tímida em relação à maneira de falar da Taiga. No quinto volume do romance leve, ela fica preocupada em entrar em um relacionamento que o leve ao casamento antes de completar 30 anos, mas quando esse aniversário chega, ela desenvolve uma personalidade distorcida e deprimida, que é ainda mais agravada sempre que Taiga menciona que é solteira .`
+    }
+}
